7ae9471760f509aacd6afb1a6b6d7763428ffbb8
logan <hahayupbiz@gmail.com>
2025-01-13 15:02:16 -0600

    abstract network printing into function

--- publicwaves-embedded/publicwaves-embedded/publicwaves-embedded.ino
+++ publicwaves-embedded/publicwaves-embedded/publicwaves-embedded.ino
@@ -5,20 +5,38 @@
 
 
 void setup() {
-  // put your setup code here, to run once:
+  // initiate serial port
   Serial.begin(9600);
+  
+  Serial.println("--Initiating publicwaves-embedded--");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  printWifiNetworks();
+
+}
+
+void printWifiNetworks() {
+
+  // gets the number of wifi networks in the current scan
   int numSSID = WiFi.scanNetworks();
   
+  // iterates through each network on current scan
   for(int i = 0; i < numSSID; i++) {
+
+    // amount of milliseconds passed since program has started
+    Serial.print(millis());
+    Serial.print(" ");
+
     Serial.print(i);
     Serial.print(". ");
+
     Serial.print(WiFi.SSID(i));
+
     Serial.print(", Signal: ");
     Serial.print(WiFi.RSSI(i)); 
+
     Serial.println("");
   }
   Serial.println("");
